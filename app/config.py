59bc2b485f71b88e90202e01091455a580038af4
KeerthanaBurada <keerthanaburada.11@gmail.com>
2026-06-08 18:50:10 +0530

    Modified session_service.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SESSION_SERVICE_URL = os.getenv(
     "SESSION_SERVICE_URL",
--- app/services/session_service.py
+++ app/services/session_service.py
@@ -20,3 +20,20 @@ def get_session_payload(session_id: str):
     response.raise_for_status()
 
     return response.json()["payload_sent"]
+
+def get_session_payload(session_id: str):
+
+    print("URL:", f"{SESSION_SERVICE_URL}/api/sessions/{session_id}/send")
+    print("HEADERS:", headers)
+
+    response = requests.post(
+        f"{SESSION_SERVICE_URL}/api/sessions/{session_id}/send",
+        headers=headers
+    )
+
+    print("STATUS:", response.status_code)
+    print("BODY:", response.text)
+
+    response.raise_for_status()
+
+    return response.json()["payload_sent"]
